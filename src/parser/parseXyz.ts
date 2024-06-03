c45a610bd3acfbd498b1ddbc6257973acf5eaeca
Mallik Cheripally <mallikcheripally@gmail.com>
2024-06-03 19:37:30 +0530

    feat: Change parseXyz output to object format
    Updated the parseXyz function and corresponding tests to return XYZ values as an object instead of an array. This provides a more intuitive mapping of the color strings to their corresponding XYZ components.

--- src/parser/parseXyz.ts
+++ src/parser/parseXyz.ts
@@ -5,15 +5,19 @@ import { xyzRegex } from '@/utils/regex';
  * Parses an XYZ color string and returns the corresponding XYZ values.
  *
  * @param {string} color - The XYZ color string in the format "xyz(x, y, z)".
- * @returns {[number, number, number]} - The parsed XYZ values.
+ * @returns {{ x: number; y: number; z: number; }} - An object containing XYZ values
  * @throws {Error} Throws an error if the color format is invalid.
  */
-export function parseXyz(color: string): [number, number, number] {
+export function parseXyz(color: string): { x: number; y: number; z: number; } {
     const match = color.match(xyzRegex);
 
     if (!match || !isValidXyz(color)) {
         throw new Error('Invalid XYZ color format');
     }
 
-    return [parseFloat(match[1]), parseFloat(match[3]), parseFloat(match[5])];
+    return {
+        x: parseFloat(match[1]),
+        y: parseFloat(match[3]),
+        z: parseFloat(match[5])
+    };
 }
--- tests/parser/parseXyz.test.ts
+++ tests/parser/parseXyz.test.ts
@@ -2,13 +2,13 @@ import { parseXyz } from '@/parser/parseXyz';
 
 describe('parseXyz', () => {
     test('parses valid XYZ color strings', () => {
-        expect(parseXyz('xyz(41.24, 21.26, 1.93)')).toEqual([41.24, 21.26, 1.93]);
-        expect(parseXyz('xyz(0, 0, 0)')).toEqual([0, 0, 0]);
-        expect(parseXyz('xyz(95.047, 100.000, 108.883)')).toEqual([95.047, 100.000, 108.883]);
+        expect(parseXyz('xyz(41.24, 21.26, 1.93)')).toEqual({ x: 41.24, y: 21.26, z: 1.93});
+        expect(parseXyz('xyz(0, 0, 0)')).toEqual({ x: 0, y: 0, z: 0 });
+        expect(parseXyz('xyz(95.047, 100.000, 108.883)')).toEqual({ x: 95.047, y: 100.000, z: 108.883 });
     });
 
     test('parses XYZ color strings with negative values', () => {
-        expect(parseXyz('xyz(-41.24, -21.26, -1.93)')).toEqual([-41.24, -21.26, -1.93]);
+        expect(parseXyz('xyz(-41.24, -21.26, -1.93)')).toEqual({ x: -41.24, y: -21.26, z: -1.93 });
     });
 
     test('throws error for invalid XYZ color formats', () => {
